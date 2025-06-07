2ce414e8861971e206a25c64e2a2c66895aff057
khwahish-pandey <1ds23et057@dsce.edu.in>
2025-06-07 23:23:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
   return `${formattedMins}:${formattedSecs}`;
 }
 async function getsong() {
-  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let a = await fetch("http://127.0.0.1:5500/songs/public");
   let response = await a.text();
   console.log(response);
 
